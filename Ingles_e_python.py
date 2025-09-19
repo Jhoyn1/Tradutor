88bb9113f28e7934b2c7329b2bdaea9bf9c65e48
Miguel Damazio <grishnakhtheoden@gmail.com>
2025-09-19 07:54:33 -0300

    Tradutor
    Um tradutor simples.

---
+++ Ingles_e_python.py
@@ -0,0 +1,46 @@
+from deep_translator import GoogleTranslator
+
+# dicionário para guardar palavras já traduzidas
+memoria = {}
+
+# criando um loop infinito, até usar a string "break"
+while True:
+
+    direcao = input("Digite 1 para traduzir do EN -> PT, 2 para PT -> EN, ou sair para encerrar:")
+
+
+    #if = se
+    #break, citado anteriormente, para o "while True"
+    if direcao == "sair": 
+        print("você que manda patrão")
+        break
+
+
+    #aqui eu crio a variavel "palavra" e faço ela ser igual ao que o usuario colocar. O input é usado para 
+    #aparecer uma mensagem para o usuario. O strip(). 
+    #O .strip é usado para manter as palavras sem espaços extra,  no inicio e no fim.
+    #E o .lower deixa todas as letras minusculas 
+    palavra = input("qual palavra você quer traduzir:").strip().lower()
+
+    #aqui eu vejo se a palavra pedida está na memória, e mostro se ela já estava,
+    # não precisando usar a internet para traduzir
+    if palavra in memoria:
+        print(f"Sua palavra é '{palavra}'sua palavra já está salva e é: {memoria[palavra]}")
+
+    #else = se não
+    #direcao 1 vai de ingles pra portugues
+    #direcao 2 vai de portugues pra ingles
+    #elfi = senão se
+    #Basicamente, ele ve a direcao que vc escolher e traduz, depois mostra tanto a palavra que quer 
+    #traduzir quanto a tradução, além de guardar a nova palavra na memoria
+    else:
+        if direcao == "1": 
+            traducao = GoogleTranslator(source="en", target="pt").translate(palavra)
+        elif direcao == "2":
+            traducao = GoogleTranslator(source="pt", target="en").translate(palavra)
+        else:
+            print("Opcão invalida")
+    memoria[palavra] = traducao
+    print(f"A tradução de '{palavra}' é: {traducao}")
+
+
