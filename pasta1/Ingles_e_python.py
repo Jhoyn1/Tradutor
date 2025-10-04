87e5aeb85e2f0ebfc1251e9d1bc3c0857d8ad8d9
Miguel Damazio <grishnakhtheoden@gmail.com>
2025-10-04 17:21:50 -0300

    Tradução com banco de dados
    Eu demorei um pouco, mas o q eu queria está feito.

--- pasta1/Ingles_e_python.py
+++ pasta1/Ingles_e_python.py
@@ -1,27 +1,38 @@
-"""
+import json
+import os
 from deep_translator import GoogleTranslator
 
 # dicionário para guardar palavras já traduzidas
 memoria = {}
 
+
+arquivo = "dicionario.json"
+
+if os.path.exists(arquivo):
+    with open(arquivo, "r", encoding="utf-8") as f:
+        memoria = json.load(f)
+else:
+    memoria = {}
+
+
 # criando um loop infinito, até usar a string "break"
 while True:
 
-    direcao = input("Digite 1 para traduzir do EN -> PT, 2 para PT -> EN, ou sair para encerrar:")
+    direcao = input("Digite 1 para traduzir do EN -> PT, 2 para PT -> EN, ou sair para encerrar: ")
 
 
     #if = se
-    #break, citado anteriormente, para o "while True"
+    #break, citado anteriormente. Para (stop) o "while True"
     if direcao == "sair": 
-        print("você que manda patrão")
+        print("Você que manda patrão")
         break
 
 
     #aqui eu crio a variavel "palavra" e faço ela ser igual ao que o usuario colocar. O input é usado para 
-    #aparecer uma mensagem para o usuario. O strip(). 
-    #O .strip é usado para manter as palavras sem espaços extra,  no inicio e no fim.
+    #aparecer uma mensagem para o usuario. 
+    #O .strip é usado para manter as palavras sem espaços extra, no inicio e no fim.
     #E o .lower deixa todas as letras minusculas 
-    palavra = input("qual palavra você quer traduzir:").strip().lower()
+    palavra = input("qual palavra você quer traduzir: ").strip().lower()
 
     #aqui eu vejo se a palavra pedida está na memória, e mostro se ela já estava,
     # não precisando usar a internet para traduzir
@@ -41,22 +52,18 @@ while True:
             traducao = GoogleTranslator(source="pt", target="en").translate(palavra)
         else:
             print("Opcão invalida")
-    memoria[palavra] = traducao
-    print(f"A tradução de '{palavra}' é: {traducao}")
+            continue
 
-"""
+        memoria[palavra] = traducao
+        print(f"A tradução de '{palavra}' é: {traducao}")
 
-#!/usr/bin/python3
+#Um whit que é usado para abrir e fechar os arquivos com segurança
+#Aqui ele está abrindo o arquivo e com o "w" (write) ele abre para escrever mais palavras na pasta
+#O encoding serve para codificar as palavras para uma linguagem que o computador vai entender
+#"utf=8" ele continua com os acentos, letras diferentes tipo 愛 ou حب e até emojis, mantendo elas após
+#voltar ao normal
+    with open(arquivo, "w", encoding="utf=8") as f:
+        json.dump(memoria, f, ensure_ascii=False, indent=4)
 
-def hanoi(n, origem, destino, auxiliar):
-    if n == 1:
-        print("Mova disco 1 de " + origem + " para " + destino)
-    else:
-        hanoi(n - 1, origem, auxiliar, destino)
-        print("Mova disco " + str(n) + " de " + origem + " para " + destino)
-        hanoi(n - 1, auxiliar, destino, origem)
-
-# Exemplo: 3 discos
-hanoi(3, 'A', 'C', 'B')
 
 
